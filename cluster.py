3b195b5ca3f03e4d3389ef27cf0fbd23bf6f99d0
Zheng Tang <oscartang1120@gmail.com>
2021-07-20 00:15:17 -0700

    add more clustering algs, double clustering implementation

--- cluster.py
+++ cluster.py
@@ -2,7 +2,7 @@ from transformers import BertTokenizer, BertModel
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained("bert-base-uncased")
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift, AffinityPropagation, KMeans, estimate_bandwidth
 
 import numpy as np
 
@@ -23,17 +23,39 @@ def get_vectors(sentences):
         encoded_input = tokenizer(text, return_tensors='pt')
         output = model(**encoded_input)
         for i, token in enumerate(nltk.pos_tag(tokenizer.convert_ids_to_tokens(encoded_input.input_ids.data.numpy().tolist()[0]))):
-            if token[-1].startswith('VB') and token[0] not in ['[CLS]','[SEP]', '/'] and wnl.lemmatize(token[0], pos='v') not in ['get', 'be', 'have']:
+            if token[-1].startswith('VB') and token[0] not in ['[CLS]','[SEP]', '/', '[', ']', '\\'] and wnl.lemmatize(token[0], pos='v') not in ['get', 'be', 'have']:
                 vectors[token[0]].append(output.last_hidden_state[0].data.numpy()[i])
             
     return vectors
 
-def cluster(vectors):
-    # af = AffinityPropagation(preference=-50).fit(vectors)
-    # cluster_centers_indices = af.cluster_centers_indices_
-    # labels = af.labels_
-    labels = KMeans(n_clusters=1000, random_state=170).fit_predict(vectors)
-    return labels
+def cluster_af(vectors):
+
+    af = AffinityPropagation(preference=-50).fit(vectors)
+    cluster_centers = af.cluster_centers_
+    labels = af.labels_
+
+    return cluster_centers, labels
+
+def cluster_km(vectors, n):
+
+    km = KMeans(n_clusters=min(n, len(vectors)), random_state=170).fit(vectors)
+    labels = km.labels_
+    cluster_centers = km.cluster_centers_
+
+    return cluster_centers, labels
+
+def cluster_ms(vectors):
+    if len(vectors) == 1:
+        return vectors, [0]
+    bandwidth = estimate_bandwidth(vectors, quantile=0.5, n_samples=min(len(vectors), 500))
+    bandwidth = bandwidth if bandwidth!=0 else None
+    ms = MeanShift(bandwidth=bandwidth)
+    ms.fit(vectors)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
+
+
+    return cluster_centers, labels
 
 
 data_path = "/Users/zheng/Downloads/100cdr/"
@@ -45,18 +67,20 @@ for file in filenames:
             if annotation['label'] == 'qntfy-key-sentence-annotator':
                 for content in annotation['content']:
                     sentences += [content['value']]
+
 vectors_list = get_vectors(sentences)
-print (len(vectors_list))
 vectors = list()
 words = list()
 for v in vectors_list:
-    words += [v]
-    vectors += [np.mean(sample(vectors_list[v], min(4, len(vectors_list[v]))), axis=0)]
+    centers, _ = cluster_km(vectors_list[v], 4)
+    for i in centers:
+        words.append(v)
+        vectors.append(i)
 
-cluster_ids = cluster(vectors)
-clusters = defaultdict(list)
+_, cluster_ids = cluster_km(vectors, 1000)
+clusters = defaultdict(set)
 for i, c_id in enumerate(cluster_ids):
-    clusters[int(c_id)].append(words[i])
+    clusters[int(c_id)].add(words[i])
 print (json.dumps(clusters))
 
 
